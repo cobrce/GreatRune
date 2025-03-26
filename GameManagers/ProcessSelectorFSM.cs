e42ed7ea2b9cbf824b784996e5799953528940e1
COB <cob_rce@hotmail.com>
2025-03-26 06:27:47 +0100

    initial commit

---
+++ GameManagers/ProcessSelectorFSM.cs
@@ -0,0 +1,75 @@
+﻿using System.Diagnostics;
+
+namespace GreatRune.GameManagers
+{
+    internal class ProcessSelectorFSM
+    {
+        public ProcessSelectorFSM(string processName, MemoryManager memoryManager)
+        {
+            ProcessName = processName;
+            MemoryManager = memoryManager;
+        }
+
+        private enum SearchState
+        {
+            NotFound,
+            LookingForAob,
+            Found,
+            ResetSearch,
+        };
+
+        public void ResetSearch()
+        {
+            MemoryManager.Close();
+            process = null;
+            searchState = SearchState.NotFound;
+        }
+
+        public bool SearchForProcess(out Process? GameProcess)
+        {
+            bool result = false;
+            switch (searchState)
+            {
+                case SearchState.NotFound:
+                    this.process = Process.GetProcessesByName(ProcessName).FirstOrDefault();
+                    if (process != null)
+                        searchState = SearchState.LookingForAob;
+                    break;
+
+                case SearchState.LookingForAob:
+                    if (process != null && MemoryManager.Open(process))
+                    {
+                        searchState = SearchState.Found;
+                        result = true;
+                    }
+                    else
+                    {
+                        searchState = SearchState.ResetSearch;
+                    }
+                    break;
+
+                case SearchState.Found:
+                    if (process?.WaitForExit(10) == true)
+                        searchState = SearchState.ResetSearch;
+                    else
+                        result = true;
+                    break;
+
+                case SearchState.ResetSearch:
+                    ResetSearch();
+
+                    break;
+                default:
+                    break;
+            }
+            GameProcess = MemoryManager.IsOpen ? process : null;
+            return result;
+        }
+
+        private SearchState searchState = SearchState.NotFound;
+        private Process? process;
+
+        public string ProcessName { get; }
+        public MemoryManager MemoryManager { get; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using GreatRune;
+using Terminal.Gui;
+
+Application.Init();
+Application.Run(new MainDialog());
+Application.Shutdown();
